c00b6eb8a88a851126006add5aa5dbe830de9f74
Jonah Karew <jonahkarew@gmail.com>
2019-11-15 21:06:24 -0500

    added new paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  state = {
+    loginStatus: false
+  }
+
+  checkLoginStatus = () => {
+    let token = localStorage.getItem('accessToken');
+    if(token){
+      console.log ('This user is logged in.')
+      this.setState({ loginStatus: true })
+    }
+    else{
+      console.log('This user is not logged in.')
+      this.setState({ loginStatus: false })
+    }
+  }
+
+  handleLogout = () => {
+    this.setState({ loginStatus: false });
+    localStorage.removeItem('accessToken');
+    window.location.reload();
+  }
+
+ render(){
+   return(
+          <Router>
+            <React.Fragment>
+
+              <Switch exact path = '/'></Switch>
+              <Switch exact path = '/main'></Switch>
+              <Switch exact path= '/profile'></Switch>
+              <Switch exact path= '/register'></Switch>
+            </React.Fragment>
+          </Router> 
    
-    </div>
-  );
+  
+   )
+ }
 }
 
 export default App;

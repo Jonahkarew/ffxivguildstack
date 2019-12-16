73b8b7acce7e011de218a00f311e9de5055d7d5d
Jonah Karew <jonahkarew@gmail.com>
2019-12-16 14:44:05 -0500

    redirected mongo connections for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 
 // import routes from './src/routes/api'
@@ -8,13 +9,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cookieParser())
 
 const port = process.env.PORT || 3001;
 
 
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/';
 mongoose.Promise = Promise;
-mongoose.connect(mongoUri || 'mongodb://localhost:27017/', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(mongoUri, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
     return res.end('Api is working')

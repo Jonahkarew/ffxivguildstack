133f961f6f2bacc5d020062e7d71db0f002b13ea
Jonah Karew <jonahkarew@gmail.com>
2019-12-17 11:47:47 -0500

    setting state in profile component

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -12,6 +12,26 @@ class Profile extends Component {
         offHandIlvl: '',
         headName: '',
         headIlvl: '',
+        chestName: '',
+        chestIlvl: '',
+        armsName: '',
+        armsIlvl: '',
+        beltName: '',
+        beltIlvl: '',
+        pantsName: '',
+        pantsIlvl: '',
+        shoesName: '',
+        shoesIlvl: '',
+        earringName: '',
+        earringIlvl: '',
+        necklaceName: '',
+        necklaceIlvl: '',
+        wristName: '',
+        wristIlvl: '',
+        ring1Name: '',
+        ring1Ilvl: '',
+        ring2Name: '',
+        ring2Ilvl: ''
         }
     
 
@@ -23,12 +43,37 @@ class Profile extends Component {
                 this.setState({data: response.data});
                 this.setState({
                     email: response.data.email,
-                    email: this.state.data.email,
+                    // email: this.state.data.email,
                     // data.email,
                     characterName: response.data.characterName,
                     characterJob: response.data.characterJob,
                     weaponName: response.data.gear.weapon.weaponName,
-                    weaponIlvl: response.data.gear.weapon.weaponIlvl
+                    weaponIlvl: response.data.gear.weapon.weaponIlvl,
+                    offHandName: response.data.gear.offHand.offHandNameName,
+                    offHandIlvl: response.data.gear.offHand.offHandIlvl,
+                    headName: response.data.gear.head.headName,
+                    headIlvl: response.data.gear.head.headIlvl,
+                    chestName: response.data.gear.chest.chestName,
+                    chestIlvl: response.data.gear.chest.chestIlvl,
+                    armsName: response.data.gear.arms.armsName,
+                    armsIlvl: response.data.gear.arms.armsIlvl,
+                    beltName: response.data.gear.belt.beltName,
+                    beltIlvl: response.data.gear.belt.beltIlvl,
+                    pantsName: response.data.gear.pants.pantsName,
+                    pantsIlvl: response.data.gear.pants.pantsIlvl,
+                    shoesName: response.data.gear.shoes.shoesName,
+                    shoesIlvl: response.data.gear.shoes.shoesIlvl,
+                    earringName: response.data.gear.earring.earringName,
+                    earringIlvl: response.data.gear.earring.earringIlvl,
+                    necklaceName: response.data.gear.neacklace.necklaceName,
+                    neacklaceIlvl: response.data.gear.necklace.necklaceIlvl,
+                    wristName: response.data.gear.wrist.wristName,
+                    wristIlvl: response.data.gear.wrist.wristIlvl,
+                    ring1Name: response.data.gear.ring1.ring1Name,
+                    ring1Ilvl: response.data.gear.ring1.ring1Ilvl,
+                    ring2Name: response.data.gear.ring2.ring2Name,
+                    ring2Ilvl: response.data.gear.ring2.ring2Ilvl
+
                 })
             })
             .catch((err) => console.log(err))

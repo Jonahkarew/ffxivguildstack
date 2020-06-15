a0aa3b3dc3c141ec4fdaab555ee7a8fee3d9f257
Jonah Karew <jonahkarew@gmail.com>
2020-06-15 11:40:04 -0400

    pre second proxy commit

--- client/src/Components/News/News.js
+++ client/src/Components/News/News.js
@@ -2,65 +2,68 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import cheerio from 'cheerio';
 function News() {
+    const [photos, setPhotos] = useState([]);
 
-    const getNews = () => {
-        let url = `https://na.finalfantasyxiv.com/lodestone/`
-        axios.get(url, {
+    // const getNews = () => {
+    //     let url = `https://na.finalfantasyxiv.com/lodestone/`
+    //     axios.get(url, {
             
-                headers: {
-                    'Accept': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                  }
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Access-Control-Allow-Origin': '*',
+    //               }
             
-        }).then(response => {
-            const $ = cheerio.load(response.data);
-            let json = []
-            $(".news__list--topics").each(
-                function (){
-                    var newNews = {
-                        title: $(this).children().first().text(),
-                        image: $(this).children(".news__list--banner").children().first().children().first().attr("src"),
-                        externalLink: `https://na.finalfantasyxiv.com/${$(this).children(".news__list--banner").children("a").attr("href")}`,
-                        description: $(this).children(".news__list--banner").children(".mdl-text__xs-m16").text()
-                    }
-                    json.push(newNews)
+    //     }).then(response => {
+    //         const $ = cheerio.load(response.data);
+    //         let json = []
+    //         $(".news__list--topics").each(
+    //             function (){
+    //                 var newNews = {
+    //                     title: $(this).children().first().text(),
+    //                     image: $(this).children(".news__list--banner").children().first().children().first().attr("src"),
+    //                     externalLink: `https://na.finalfantasyxiv.com/${$(this).children(".news__list--banner").children("a").attr("href")}`,
+    //                     description: $(this).children(".news__list--banner").children(".mdl-text__xs-m16").text()
+    //                 }
+    //                 json.push(newNews)
     
     
-                }
-            )
-            // return json
-            console.log("getNews() " + json)
-        })
-    }
+    //             }
+    //         )
+    //         // return json
+    //         console.log("getNews() " +json)
+    //     })
+    // }
 
-    const fetchNews = () => {
-        const options = {
-            method: 'GET',
-            headers: {
-                // 'Accept': 'application/json',
-                // 'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-            }
-        }
-        fetch("https://na.finalfantasyxiv.com/lodestone/", options)
-        .then(res => {
-            console.log("fetchNews() " + res)
-        })
-    } 
+    // const fetchNews = () => {
+    //     const options = {
+    //         method: 'GET',
+    //         headers: {
+    //             // 'Accept': 'application/json',
+    //             // 'Content-Type': 'application/json;charset=UTF-8',
+    //             "Access-Control-Allow-Origin": "*",
+    //             "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+    //             "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    //         }
+    //     }
+    //     fetch("https://na.finalfantasyxiv.com/lodestone/", options)
+    //     .then(res => {
+    //         console.log("fetchNews() " + res)
+    //     })
+    // } 
 
     const checkNews = () => {
         console.log("this here")
-        axios.get("https://google.com", 
-                    {type: "GET",}
-                    ).then(res => {console.log(res)})
+        axios.get("https://giphy.com/gifs/UX4U37Y9mdz3BXNbcA").then(res => {
+            const photos = res.data;
+            setPhotos(photos)
+            console.log(photos)
+        })
     }
 
 
     useEffect(() => {
-        getNews()
-        fetchNews()
+        // getNews()
+        // fetchNews()
         checkNews()
     })
 

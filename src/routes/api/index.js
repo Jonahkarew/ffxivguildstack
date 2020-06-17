c467a439c237477cc69be8e869896d26f9d9f8c9
Jonah Karew <jonahkarew@gmail.com>
2020-06-17 18:07:13 -0400

    removed references to deprecated routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postBlog = require('./post-routes');
-const newsRoute = require('./news-routes')
 
 router.use('/user', userRoutes);
 router.use('/user', postBlog);
-router.use('/user', newsRoute)
 
 module.exports = router;

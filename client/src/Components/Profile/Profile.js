764e33620c2bf4a917bc6713be42cb5af7f1f09e
Jonah Karew <jonahkarew@gmail.com>
2020-08-18 20:34:35 -0400

    editing profile.js to perform api call rather than scrape on the front end

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -449,6 +449,185 @@ class Profile extends Component {
         })
     }
 
+    clickyBoi = () => {
+        fetch(`/api/user/scrape/${this.state.characterName}/${this.state.characterWorld}`)
+        .then(r => r.json())
+        .then(res => {
+            console.log(res)
+            let ohIlvl;
+            if (res.offHandIlvl === NaN){
+                ohIlvl = 0
+            } else{
+                ohIlvl = res.offHandIlvl
+            }
+
+            this.setState(  {
+                characterName: res.characterName,
+                characterTitle: res.characterTitle,
+                characterLevel: res.characterLevel,
+                characterPicture: res.characterPicture,
+                strength: res.strength,
+                dexterity: $(res).find(".character__param__list tr td").eq(1).text(),
+                vitality: $(res).find(".character__param__list tr td").eq(2).text(),
+                intelligence: $(res).find(".character__param__list tr td").eq(3).text(),
+                mind: $(res).find(".character__param__list tr td").eq(4).text(),
+                criticalHitRate: $(res).find(".character__param__list tr td").eq(5).text(),
+                determination: $(res).find(".character__param__list tr td").eq(6).text(),
+                directHitRate: $(res).find(".character__param__list tr td").eq(7).text(),
+                defense: $(res).find(".character__param__list tr td").eq(8).text(),
+                magicDefense: $(res).find(".character__param__list tr td").eq(9).text(),
+                attackPower: $(res).find(".character__param__list tr td").eq(10).text(),
+                skillSpeed: $(res).find(".character__param__list tr td").eq(11).text(),
+                attackMagicPotency: $(res).find(".character__param__list tr td").eq(12).text(),
+                healingMagicPotency: $(res).find(".character__param__list tr td").eq(13).text(),
+                spellSpeed: $(res).find(".character__param__list tr td").eq(14).text(),
+                tenacity: $(res).find(".character__param__list tr td").eq(15).text(),
+                piety: $(res).find(".character__param__list tr td").eq(16).text(),
+                weaponName: $(res).find(".db-tooltip__item__name").eq(0).text(),
+                weaponIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(0).text().replace("Item Level ", "")),
+                offHandName: $(res).find(`.character__detail__icon`).eq(1)
+                    .children()
+                    .first()
+                    .children(".item_detail_box")
+                    .children()
+                    .first()
+                    .children(".popup_w412_body_gold")
+                    .children().first()
+                    .children().first()
+                    .children(".db-tooltip__item__txt")
+                    .children("h2")
+                    .text(),
+                offHandIlvl: ohIlvl,
+                headName: $(res).find(".db-tooltip__item__name").eq(1).text(),
+                headIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(1).text().replace("Item Level ", "")),
+                chestName: $(res).find(".db-tooltip__item__name").eq(2).text(),
+                chestIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(2).text().replace("Item Level ", "")),
+                armsName: $(res).find(".db-tooltip__item__name").eq(3).text(),
+                armsIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(3).text().replace("Item Level ", "")),
+                beltName: $(res).find(".db-tooltip__item__name").eq(4).text(),
+                beltIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(4).text().replace("Item Level ", "")),
+                pantsName: $(res).find(".db-tooltip__item__name").eq(5).text(),
+                pantsIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(5).text().replace("Item Level ", "")),
+                shoesName: $(res).find(".db-tooltip__item__name").eq(6).text(),
+                shoesIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(6).text().replace("Item Level ", "")),
+                earringName: $(res).find(".db-tooltip__item__name").eq(7).text(),
+                earringIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(7).text().replace("Item Level ", "")),
+                necklaceName: $(res).find(".db-tooltip__item__name").eq(8).text(),
+                necklaceIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(8).text().replace("Item Level ", "")),
+                wristName: $(res).find(".db-tooltip__item__name").eq(9).text(),
+                wristIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(9).text().replace("Item Level ", "")),
+                ring1Name: $(res).find(".db-tooltip__item__name").eq(10).text(),
+                ring1Ilvl: parseInt($(res).find(".db-tooltip__item__level").eq(10).text().replace("Item Level ", "")),
+                ring2Name: $(res).find(".db-tooltip__item__name").eq(11).text(),
+                ring2Ilvl: parseInt($(res).find(".db-tooltip__item__level").eq(11).text().replace("Item Level ", "")),
+                stats: {
+                    attributes: {
+                        strength: parseInt($(res).find(".character__param__list tr td").eq(0).text()),
+                        dexterity: $(res).find(".character__param__list tr td").eq(1).text(),
+                        vitality: $(res).find(".character__param__list tr td").eq(2).text(),
+                        intelligence: $(res).find(".character__param__list tr td").eq(3).text(),
+                        mind: $(res).find(".character__param__list tr td").eq(4).text(),
+                    },
+                    subAttributes: {
+                        criticalHitRate: $(res).find(".character__param__list tr td").eq(5).text(),
+                        determination: $(res).find(".character__param__list tr td").eq(6).text(),
+                        directHitRate: $(res).find(".character__param__list tr td").eq(7).text(),
+                    },
+                    defensiveProperties: {
+                        defense: $(res).find(".character__param__list tr td").eq(8).text(),
+                        magicDefense: $(res).find(".character__param__list tr td").eq(9).text(),
+                    },
+                    physicalProperties: {
+                        attackPower: $(res).find(".character__param__list tr td").eq(10).text(),
+                        skillSpeed: $(res).find(".character__param__list tr td").eq(11).text(),
+                    },
+                    mentalProperties: {
+                        attackMagicPotency: $(res).find(".character__param__list tr td").eq(12).text(),
+                        healingMagicPotency: $(res).find(".character__param__list tr td").eq(13).text(),
+                        spellSpeed: $(res).find(".character__param__list tr td").eq(14).text(),
+                    },
+                    role: {
+                        tenacity: $(res).find(".character__param__list tr td").eq(15).text(),
+                        piety: $(res).find(".character__param__list tr td").eq(16).text()
+                    }
+                },
+                gear: {
+                    weapon: {
+                        weaponName: $(res).find(".db-tooltip__item__name").eq(0).text(),
+                        weaponIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(0).text().replace("Item Level ", "")),
+                    },
+                    offHand: {
+                        offHandName: $(res).find(`.character__detail__icon`).eq(1)
+                            .children()
+                            .first()
+                            .children(".item_detail_box")
+                            .children()
+                            .first()
+                            .children(".popup_w412_body_gold")
+                            .children().first()
+                            .children().first()
+                            .children(".db-tooltip__item__txt")
+                            .children("h2")
+                            .text(),
+                        offHandIlvl:
+                            parseInt($(res).find(`.character__detail__icon`).eq(1)
+                                .children()
+                                .first()
+                                .children(".item_detail_box")
+                                .children()
+                                .first()
+                                .children(".popup_w412_body_gold")
+                                .children(".db-tooltip__item__level")
+                                .text().replace("Item Level ", "")),
+                    },
+                    head: {
+                        headName: $(res).find(".db-tooltip__item__name").eq(1).text(),
+                        headIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(1).text().replace("Item Level ", "")),
+                    },
+                    chest: {
+                        chestName: $(res).find(".db-tooltip__item__name").eq(2).text(),
+                        chestIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(2).text().replace("Item Level ", "")),
+                    },
+                    arms: {
+                        armsName: $(res).find(".db-tooltip__item__name").eq(3).text(),
+                        armsIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(3).text().replace("Item Level ", "")),
+                    },
+                    belt: {
+                        beltName: $(res).find(".db-tooltip__item__name").eq(4).text(),
+                        beltIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(4).text().replace("Item Level ", "")),
+                    },
+                    pants: {
+                        pantsName: $(res).find(".db-tooltip__item__name").eq(5).text(),
+                        pantsIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(5).text().replace("Item Level ", "")),
+                    },
+                    shoes: {
+                        shoesName: $(res).find(".db-tooltip__item__name").eq(6).text(),
+                        shoesIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(6).text().replace("Item Level ", "")),
+                    },
+                    earring: {
+                        earringName: $(res).find(".db-tooltip__item__name").eq(7).text(),
+                        earringIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(7).text().replace("Item Level ", "")),
+                    },
+                    necklace: {
+                        necklaceName: $(res).find(".db-tooltip__item__name").eq(8).text(),
+                        necklaceIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(8).text().replace("Item Level ", "")),
+                    },
+                    wrist: {
+                        wristName: $(res).find(".db-tooltip__item__name").eq(9).text(),
+                        wristIlvl: parseInt($(res).find(".db-tooltip__item__level").eq(9).text().replace("Item Level ", "")),
+                    },
+                    ring1: {
+                        ring1Name: $(res).find(".db-tooltip__item__name").eq(10).text(),
+                        ring1Ilvl: parseInt($(res).find(".db-tooltip__item__level").eq(10).text().replace("Item Level ", "")),
+                    },
+                    ring2: {
+                        ring2Name: $(res).find(".db-tooltip__item__name").eq(11).text(),
+                        ring2Ilvl: parseInt($(res).find(".db-tooltip__item__level").eq(11).text().replace("Item Level ", "")),
+                    }
+                }
+            })
+        })
+    }
 
     render() {
         var itemStyle = {
@@ -479,6 +658,9 @@ class Profile extends Component {
         }
         return (
             <div style={gridRoot}>
+                {/* test button for api route remove later */}
+                <Button button color="primary" onClick={this.clickyBoi} style={buttonStyle}>this here clicky boy</Button>
+                {/* ================================*************=================================== */}
                 <Grid container spacing={3}>
                     <Grid item  xs={6} md={4}>
                         <Paper style={{whiteSpace: "nowrap", textAlign: "center"}}>
